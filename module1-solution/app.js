1628142ead71b603323ac62796eb047edc18053f
specialm5 <saif111176@gmail.com>
2024-01-04 02:25:42 +0300

    First solution

---
+++ module1-solution/app.js
@@ -0,0 +1,37 @@
+(function(){
+'use strict';
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject=['$scope'];
+
+function LunchCheckController($scope){
+    $scope.CheckIfTooMuch=function(){
+        $scope.name=document.getElementById("lunch-menu").value;
+        $scope.message="";
+
+        if($scope.name.length==0){
+            return $scope.myObj={
+                "message":$scope.message="Please enter data first",
+                "color":"red"
+            };
+        }
+        else if($scope.name.split(",").length<=3){
+            return $scope.myObj={
+                "message":$scope.message="Enjoy!",
+                "color":"green"
+
+            };
+        }
+        else{
+            return $scope.myObj={
+                "message":$scope.message="Too much!",
+                "color":"red"
+            };
+        }
+    }
+}
+})()
+
+
+
+
